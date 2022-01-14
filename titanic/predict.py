aedc308b55a536ce694840bcfea19a37efb51b0b
cshyo1004 <cshyo1004@gmail.com>
2022-01-14 15:00:33 +0900

    재정리

--- titanic/predict.py
+++ titanic/predict.py
@@ -3,6 +3,7 @@ import pandas as pd
 from xgboost import XGBClassifier
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import accuracy_score
+from catboost import CatBoostClassifier, Pool
 import warnings
 warnings.filterwarnings('ignore')
 
@@ -11,8 +12,7 @@ train = pd.read_csv('train.csv')
 test = pd.read_csv('test.csv')
 submission = pd.read_csv('submission.csv')
 
-# 데이터 전처리
-## 결측치 제거
+## 결측치 확인
 train.isnull().sum()
 test.isnull().sum()
 '''
@@ -26,6 +26,8 @@ Age : 86 / Pclass에 따라서 Age를 짐작할 수 있다고 판단하여, Pcla
 Cabin : 327 / Pclass에 따라서 Cabin을 짐작할 수 있다고 판단하여, Pclass에 따른 Cabin의 분포별 값으로 대체
 Fare : 1 / 0으로 대체(중요도 낮음)
 '''
+
+# 데이터 전처리
 ## Pclass별 평균 Age 값으로 대체
 age_by_pclass_train = {x : float(int(train[train['Pclass']==x]['Age'].mean())) for x in train['Pclass'].unique()}
 age_by_pclass_test = {x : float(int(test[test['Pclass']==x]['Age'].mean())) for x in test['Pclass'].unique()}
@@ -97,55 +99,67 @@ cabin_by_pid.update(nan_cabin)
 ## dataframe에 최종 cabin 값 업데이트
 test['Cabin'] = list(cabin_by_pid.values())
 
-## Embarked == nan, 제거
+## Embarked 데이터 삭제
 train.dropna(subset=['Embarked'], inplace=True)
 train.isnull().sum()
+
 ## Fare 데이터 0으로 대체
-test[test['Fare'].isnull()] = 0
-# 이상값 대체
-test.loc[test[test['Sex'] == 0].index[0],'Sex'] = 'female'
-# 성별 데이터 one-hot encoding
-train['Sex'] = train['Sex'].map({'female':0,'male':1})
-test['Sex'] = test['Sex'].map({'female':0,'male':1})
-# 데이터 확인
+test['Fare'].fillna(0, inplace=True)
+
+## Fare 데이터 dtype int형으로 변경
+train['Fare'] = train['Fare'].astype(int)
+test['Fare'] = test['Fare'].astype(int)
+
+## PassengerId 삭제
+train.drop(columns=['PassengerId'], inplace=True)
+test.drop(columns=['PassengerId'], inplace=True)
+
+## 데이터 확인
 train.info()
 test.info()
 
-## 데이터 정제
+# 범주형 데이터 전처리
+'''
+Name : 삭제
+Sex : encoding
+Ticket : 삭제
+Cabin : encoding
+Embarked : encoding
+'''
+train.drop(columns=['Name','Ticket'], inplace=True)
+test.drop(columns=['Name','Ticket'], inplace=True)
+# encoding
+## Sex
+concat_sex_train = pd.get_dummies(train['Sex'], prefix='Sex')
+concat_sex_test = pd.get_dummies(test['Sex'], prefix='Sex')
+## Cabin
+concat_cabin_train = pd.get_dummies(train['Cabin'], prefix='Cabin')
+concat_cabin_test = pd.get_dummies(test['Cabin'], prefix='Cabin')
+## Embarked
+concat_embarked_train = pd.get_dummies(train['Embarked'], prefix='Embarked')
+concat_embarked_test = pd.get_dummies(test['Embarked'], prefix='Embarked')
+# concat
+train = pd.concat([train, concat_sex_train, concat_cabin_train, concat_embarked_train], axis=1)
+train.drop(columns=['Sex','Cabin','Embarked'], inplace=True)
+test = pd.concat([test, concat_sex_test, concat_cabin_test, concat_embarked_test], axis=1)
+test.drop(columns=['Sex','Cabin','Embarked'], inplace=True)
+
+# 모델 학습
+xtrain, ytrain, xtest, ytest = train_test_split(train.drop(columns=['Survived']), train.loc[:, 'Survived'], test_size=0.3, random_state=1, stratify=train.loc[:, 'Survived'])
+train_pool = Pool(xtrain, xtest, cat_features=np.where(xtrain.columns)[0])
+eval_pool = Pool(ytrain, ytest, cat_features=np.where(ytrain.columns)[0])
+model = CatBoostClassifier(eval_metric='AUC', use_best_model=True)
+model.fit(train_pool, eval_set=eval_pool)
+prediction = model.predict(ytrain)
+print(f'train score : {model.score(xtrain, xtest)}')
+print(f'model eval : {accuracy_score(ytest, prediction)}')
+
+# 결과
 x_train = train.drop(columns=['Survived'])
 x_test = train.loc[:, 'Survived']
 y_train = test
+y_train['Cabin_T'] = 0
 
-x_train['Name'] = x_train['Name'].astype('category')
-x_train['Ticket'] = x_train['Ticket'].astype('category')
-x_train['Cabin'] = x_train['Cabin'].astype('category')
-x_train['Embarked'] = x_train['Embarked'].astype('category')
-y_train['Name'] = y_train['Name'].astype('category')
-y_train['Ticket'] = y_train['Ticket'].astype('category')
-y_train['Cabin'] = y_train['Cabin'].astype('category')
-y_train['Embarked'] = y_train['Embarked'].astype('category')
-
-## 모델 검증
-xtrain, ytrain, xtest, ytest = train_test_split(x_train, x_test, test_size=0.3, random_state=1, stratify=x_test)
-
-model = XGBClassifier(tree_method='gpu_hist', enable_categorical=True)
-evals = [(ytrain,ytest)]
-params = {'max_depth':[5,7,8], 'min_child_weight':[1,3], 'learning_rate':[0.05,0.1,0.5], 'n_estimators':[200,300,400]}
-gridcv = GridSearchCV(model, param_grid=params)
-gridcv.fit(xtrain, xtest, early_stopping_rounds=30, eval_set=evals)
-print(gridcv.best_params_)
-
-model = XGBClassifier(n_estimators=200, max_depth=5, learning_rate=0.05, min_child_weight=1, tree_method='gpu_hist', enable_categorical=True)
-model.fit(xtrain, xtest, early_stopping_rounds=100, eval_set=evals, verbose=True)
-prediction = model.predict(ytrain)
-print(f'train score : {model.score(xtrain,xtest)}')
-print(f'model eval : {accuracy_score(ytest, prediction)}')
-
-## 모델 적용
-model = XGBClassifier(n_estimators=200, max_depth=5, learning_rate=0.05, min_child_weight=1, tree_method='gpu_hist', enable_categorical=True)
-model.fit(x_train, x_test)
 prediction = model.predict(y_train)
-
-# 결과
 submission['Survived'] = prediction
-submission.to_csv('submission_XGB.csv', index=False)
+submission.to_csv('submission_CAT.csv', index=False)
